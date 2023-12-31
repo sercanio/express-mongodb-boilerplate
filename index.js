1e83665608577b15b3b8d6cd3c4abafc0c605bcd
Sercan Ateş <sercan+ates@users.noreply.github.com>
2023-12-31 10:48:16 +0300

    Sequence server initialization after a successful database connection

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ require("dotenv").config();
 
 module.exports = function main(options, cb) {
   // Set default options
-  const ready = cb || function () {};
+  const ready = cb || function() { };
   const opts = Object.assign(
     {
       // Default options
@@ -48,129 +48,129 @@ module.exports = function main(options, cb) {
 
   db.once("open", () => {
     logger.info("Connected to MongoDB");
-  });
 
-  // Create the express app
-  const app = express();
+    // Create the express app
+    const app = express();
 
-  // Helmet
-  app.use(
-    helmet({
-      contentSecurityPolicy: {
-        directives: {
-          "img-src": ["'self'", "*.unsplash.com", "*.w3.org"],
-          "script-src": ["'self'", "cdn.jsdelivr.net"],
-          "style-src": ["'self'", "cdn.jsdelivr.net"],
+    // Helmet
+    app.use(
+      helmet({
+        contentSecurityPolicy: {
+          directives: {
+            "img-src": ["'self'", "*.unsplash.com", "*.w3.org"],
+            "script-src": ["'self'", "cdn.jsdelivr.net"],
+            "style-src": ["'self'", "cdn.jsdelivr.net"],
+          },
         },
-      },
-    }),
-  );
-
-  // Template engine
-  app.engine("pug", pug.renderFile);
-  app.set("views", path.join(__dirname, "views"));
-  app.set("view engine", "pug");
-
-  app.use(pinoHttp({ logger }));
-  app.use(bodyParser.json());
-  app.use(express.urlencoded({ extended: false }));
-
-  app.use(
-    session({
-      secret: process.env.SECRET_KEY, // Change this to a secure random string
-      resave: false,
-      saveUninitialized: true,
-      store: MongoStore.create({ mongoUrl: process.env.MONGODB_URI }),
-      cookie: {
-        maxAge: +process.env.SESSION_COOKIE_MAXAGE,
-        secure: false, // Set to true if your app is served over HTTPS
-        httpOnly: true,
-      },
-    }),
-  );
-  app.use(flash());
-  // Passport Configuration
-  app.use(passport.initialize());
-  app.use(passport.session());
-
-  const handleAuthentication = async (req, username, password, done) => {
-    try {
-      const user = await User.findOne({ username });
-
-      if (!user) {
-        req.flash("error", "Incorrect username or password");
-        return done(null, false);
-      }
-
-      const passwordMatch = await bcrypt.compare(password, user.password);
-
-      if (!passwordMatch) {
-        req.flash("error", "Incorrect username or password");
-        return done(null, false);
-      }
-
-      return done(null, user);
-    } catch (error) {
-      return done(error);
-    }
-  };
-
-  const authUser = async (req, username, password, done) => {
-    try {
-      await handleAuthentication(req, username, password, done);
-    } catch (error) {
-      return done(error);
-    }
-  };
-
-  passport.use(
-    new LocalStrategy(
-      {
-        usernameField: "username",
-        passwordField: "password",
-        passReqToCallback: true,
-      },
-      authUser,
-    ),
-  );
-
-  passport.serializeUser((userObj, done) => {
-    done(null, userObj);
-  });
-  passport.deserializeUser((userObj, done) => {
-    done(null, userObj);
-  });
-  app.use(middleware.sendAuthStatus);
-
-  // Register routes
-  require("./routes")(app, opts);
-
-  // Common error handlers
-  app.use((req, res, next) => {
-    next(new AppError(`Page not found: ${req.url}`, 404));
-  });
-
-  app.use((err, req, res, next) => {
-    res.locals.name = err.status;
-    res.locals.error = err;
-    res.status(err.status || 500).render("error");
-  });
-  // Start server only if the database connection is successful
-  server = app.listen(opts.port, opts.host, (err) => {
-    if (err) {
-      return ready(err, app, server);
-    }
-
-    // If some other error means we should close
-    if (serverClosing) {
-      return ready(new Error("Server was closed before it could start"));
-    }
-
-    serverStarted = true;
-    const addr = server.address();
-    logger.info(
-      `Server started at ${opts.host || addr.host || "localhost"}:${addr.port}`,
+      }),
     );
-    ready(err, app, server);
+
+    // Template engine
+    app.engine("pug", pug.renderFile);
+    app.set("views", path.join(__dirname, "views"));
+    app.set("view engine", "pug");
+
+    app.use(pinoHttp({ logger }));
+    app.use(bodyParser.json());
+    app.use(express.urlencoded({ extended: false }));
+
+    app.use(
+      session({
+        secret: process.env.SECRET_KEY, // Change this to a secure random string
+        resave: false,
+        saveUninitialized: true,
+        store: MongoStore.create({ mongoUrl: process.env.MONGODB_URI }),
+        cookie: {
+          maxAge: +process.env.SESSION_COOKIE_MAXAGE,
+          secure: false, // Set to true if your app is served over HTTPS
+          httpOnly: true,
+        },
+      }),
+    );
+    app.use(flash());
+    // Passport Configuration
+    app.use(passport.initialize());
+    app.use(passport.session());
+
+    const handleAuthentication = async (req, username, password, done) => {
+      try {
+        const user = await User.findOne({ username });
+
+        if (!user) {
+          req.flash("error", "Incorrect username or password");
+          return done(null, false);
+        }
+
+        const passwordMatch = await bcrypt.compare(password, user.password);
+
+        if (!passwordMatch) {
+          req.flash("error", "Incorrect username or password");
+          return done(null, false);
+        }
+
+        return done(null, user);
+      } catch (error) {
+        return done(error);
+      }
+    };
+
+    const authUser = async (req, username, password, done) => {
+      try {
+        await handleAuthentication(req, username, password, done);
+      } catch (error) {
+        return done(error);
+      }
+    };
+
+    passport.use(
+      new LocalStrategy(
+        {
+          usernameField: "username",
+          passwordField: "password",
+          passReqToCallback: true,
+        },
+        authUser,
+      ),
+    );
+
+    passport.serializeUser((userObj, done) => {
+      done(null, userObj);
+    });
+    passport.deserializeUser((userObj, done) => {
+      done(null, userObj);
+    });
+    app.use(middleware.sendAuthStatus);
+
+    // Register routes
+    require("./routes")(app, opts);
+
+    // Common error handlers
+    app.use((req, res, next) => {
+      next(new AppError(`Page not found: ${req.url}`, 404));
+    });
+
+    app.use((err, req, res, next) => {
+      res.locals.name = err.status;
+      res.locals.error = err;
+      res.status(err.status || 500).render("error");
+    });
+    // Start server only if the database connection is successful
+    server = app.listen(opts.port, opts.host, (err) => {
+      if (err) {
+        return ready(err, app, server);
+      }
+
+      // If some other error means we should close
+      if (serverClosing) {
+        return ready(new Error("Server was closed before it could start"));
+      }
+
+      serverStarted = true;
+      const addr = server.address();
+      logger.info(
+        `Server started at ${opts.host || addr.host || "localhost"}:${addr.port}`,
+      );
+      ready(err, app, server);
+    });
   });
 };
